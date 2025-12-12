f0342597710bb8dad6f98e5e5e3c684be1a2593f
darisasubhash <subbusubbu8193@gmail.com>
2025-12-12 11:51:51 +0530

    Employee Attendence check

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,20 @@
+import java.util.Random;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
         //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
         // to see how IntelliJ IDEA suggests fixing it.
-        System.out.println("Hello and welcome!");
+        System.out.println("Hello and welcome to Employee Wage Management System");
+        Random rand=new Random();
+        boolean isPresent= rand.nextBoolean();
+        if(isPresent){
+            System.out.println("Employee is Present");
+        }
+        else{
+            System.out.println("Employee is Absent");
+        }
 
 
 
