4fe0c416b8ce665da8cbfe531d7fc525e60f52fe
darisasubhash <subbusubbu8193@gmail.com>
2025-12-13 12:15:19 +0530

    totalwageofcompany

--- src/EmployeeWageBuilder.java
+++ src/EmployeeWageBuilder.java
@@ -1,25 +1,58 @@
 public class EmployeeWageBuilder {
-    public static int employeeWageByCompany(int wagePerHour, int workingDays, int workingHours) {
+
+    // Instance variables (company-specific)
+    private String company;
+    private int wagePerHour;
+    private int maxWorkingDays;
+    private int maxWorkingHours;
+    private int totalWage;
+
+    // Constructor
+    public EmployeeWageBuilder(String company, int wagePerHour,
+                          int maxWorkingDays, int maxWorkingHours) {
+        this.company = company;
+        this.wagePerHour = wagePerHour;
+        this.maxWorkingDays = maxWorkingDays;
+        this.maxWorkingHours = maxWorkingHours;
+    }
+
+    // Compute wage and SAVE it in instance variable
+    public void computeEmpWage() {
+
         int totalHours = 0;
         int totalDays = 0;
-        while (totalDays < workingDays && totalHours < workingHours) {
+
+        while (totalDays < maxWorkingDays &&
+                totalHours < maxWorkingHours) {
+
             totalDays++;
-            int role = (int) Math.floor(Math.random() * 3);
-            int workesHours = 0;
-            switch (role) {
+            int empType = (int) (Math.random() * 3);
+            int workHours;
+
+            switch (empType) {
                 case 1:
-                    workesHours = 8;
+                    workHours = 8; // Full-time
                     break;
                 case 2:
-                    workesHours = 6;
+                    workHours = 6; // Part-time
                     break;
                 default:
-                    workesHours = 0;
-                    break;
+                    workHours = 0; // Absent
             }
-            totalHours += workesHours;
+
+            totalHours += workHours;
         }
-        int totalWage = totalHours * wagePerHour;
+
+        // Save total wage
+        totalWage = totalHours * wagePerHour;
+    }
+
+    // Getter
+    public int getTotalWage() {
         return totalWage;
+    }
+
+    public String getCompany() {
+        return company;
     }
 }
